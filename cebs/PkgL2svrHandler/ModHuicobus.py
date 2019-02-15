fcb24baf0b3f81906e0a9e4cc83184ca12cd6e56
Andyorhuateng <smdzjl@sina.cn>
2019-02-15 08:26:13 +0800

    zjl@2019/2/15

--- cebs/PkgL2svrHandler/ModHuicobus.py
+++ cebs/PkgL2svrHandler/ModHuicobus.py
@@ -1,5 +1,5 @@
 '''
-Created on 2018年12月28日
+Created on 2019/2/14
 
 @author: Administrator
 '''
@@ -22,18 +22,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from   ctypes import c_uint8
+import urllib
+import urllib3
+import requests
+import socket
+import pycurl
+import io
+import certifi  #导入根证书集合，用于验证SSL证书可信和TLS主机身份
+from io import BytesIO
+from urllib.parse import urlencode
+import json
+import http.client
+import paho.mqtt.client as mqtt
+from multiprocessing import Queue, Process
+import threading
 
 
-class TupClsHuicobus(object):
+
+
+
+class TupClsHuicobusBasic(object):
     '''
     classdocs
     '''
-
-
-    def __init__(self, params):
+    _TUP_MQTT_SVR = ''
+    
+    def __init__(self):
         '''
         Constructor
         '''
+        self._TUP_MQTT_SVR = TupClsMqttThread()
+
+    def data_send(self):
+        pass
+
+
+        
+        
+#MQTT服务任务        
+class TupClsMqttThread():
+    _TUP_MQTT_HOST = ''
+    _TUP_MQTT_PORT = ''
+    
+    def __init__(self):
+        self._TUP_MQTT_HOST = None  #'localhost'  #'127.0.0.1'
+        self._TUP_MQTT_PORT = 1883
+        myThread = threading.Thread(target=self.handler_server, args=())
+        myThread.start()
+            
+    def handler_server(self):
+        client_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        client = mqtt.Client(client_id)    # ClientId不能重复，所以使用当前时间
+        client.username_pw_set("admin", "123456")  # 必须设置，否则会返回「Connected with result code 4」
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.connect(self._TUP_MQTT_HOST, port = self._TUP_MQTT_PORT, keepalive = 60)
+        client.loop_forever()
+        
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe("chat")
+
+    def on_message(self, client, userdata, msg):
+        print(msg.topic+" " + ":" + str(msg.payload))
+
+     
+    #客户端测试        
+    HOST = "127.0.0.1"
+    PORT = 1883
+    def client_test(self):
+        client = mqtt.Client()
+        client.connect(self.HOST, self.PORT, 60)
+        client.publish("chat","hello chenfulin",2)
+        client.loop_forever()        
+        
+        
+    
+if __name__ == '__main__':
+    cls = TupClsHuicobusBasic()
 
 
 
@@ -49,12 +115,8 @@ class TupClsHuicobus(object):
 
 
 
-
-
-
-
-
-
-
-
+        
+        
+        
+        
         
--- cebs/PkgL2svrHandler/ModHuijson.py
+++ cebs/PkgL2svrHandler/ModHuijson.py
@@ -4,14 +4,98 @@ Created on 2018年12月28日
 @author: Administrator
 '''
 
-class MyClass(object):
+import random
+import sys
+import time
+import json
+import os
+import re
+import urllib
+import http
+import socket
+import datetime
+import string
+import ctypes 
+import random
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from   ctypes import c_uint8
+import urllib
+import urllib3
+import requests
+import socket
+import pycurl
+import io
+import certifi  #导入根证书集合，用于验证SSL证书可信和TLS主机身份
+from io import BytesIO
+from urllib.parse import urlencode
+import json
+import http.client
+
+
+class TupClsHuijsonItf(object):
     '''
     classdocs
     '''
 
-
-    def __init__(self, params):
+    def __init__(self, svrAddr, svrPort):
         '''
         Constructor
         '''
-        
+        _TUP_HUIJSON_SVR_ADDR = svrAddr
+        _TUP_HUIJSON_SVR_PORT = svrPort
+    
+    #建立长连接的链路
+    #可能需要考虑建立一个实时接收任务进行监控
+    def socket_setup(self):
+        pass
+
+    def socket_disc(self):
+        pass
+
+    def socket_data_snd(self):
+        pass
+
+    def socket_data_rcv(self):
+        pass
+    
+    #无连接，一次性发送
+    def curl_data_snd(self, jsonInputData):
+        encoded_data = json.dumps(jsonInputData).encode('utf-8')
+        http = urllib3.PoolManager(maxsize=10, timeout=30.0, block=True)
+        r = http.request(
+            'POST',
+            self._TUP_HUIJSON_SVR_ADDR,
+            body=encoded_data, 
+            headers={'Content-Type':'application/json', 'Connection': 'close'}
+            )
+        try:
+            result = json.loads(r.data)
+        except Exception:
+            print("HUIJSON: Not fetch right feedback!")
+            result = ''
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
