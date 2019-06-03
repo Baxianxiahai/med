3c29f7bd5081154e38da3e5e54eafaaa80e66118
Andyorhuateng <smdzjl@sina.cn>
2019-06-03 12:11:46 +0800

    zjl@2019/6/3 R1.67.4

--- tup/PkgL2svrUniv/ModCebsHuicobus.py
+++ tup/PkgL2svrUniv/ModCebsHuicobus.py
@@ -20,10 +20,14 @@ class TupClsCebsHuicobusItf(TupClsHuicobusBasic, clsL1_ConfigOpr):
         TupClsHuicobusBasic.__init__(self, taskidUb=TUP_TASK_ID_HUICOBUS, taskNameUb="TASK_HUICOBUS", glParUb=glPar)
 
         #STM MATRIX
-        #业务处理部分
+        #业务处理部分 - 接收处理
         self.add_stm_combine(self._STM_ACTIVE, TUP_MSGID_HUICOBUS_L2FRAME_RCV, self.fsm_huicobus_l2frame_rcv_handler)
+
+        #业务处理部分 - 发送处理
         self.add_stm_combine(self._STM_ACTIVE, TUP_MSGID_GPAR_GET_CFG_RESP, self.fsm_huicobus_gpar_get_cfg_resp_rcv_handler)
         self.add_stm_combine(self._STM_ACTIVE, TUP_MSGID_GPAR_SET_CFG_RESP, self.fsm_huicobus_gpar_set_cfg_resp_handler)
+        
+        
         
         #START TASK
         self.fsm_set(TUP_STM_INIT)
@@ -43,7 +47,69 @@ class TupClsCebsHuicobusItf(TupClsHuicobusBasic, clsL1_ConfigOpr):
             self.msg_send(TUP_MSGID_GPAR_GET_CFG_REQ, TUP_TASK_ID_GPAR, mbuf)
         elif (cmdId == TUP_HHD_CMDID_SYS_SET_CONFIG_REQ):
             self.msg_send(TUP_MSGID_GPAR_SET_CFG_REQ, TUP_TASK_ID_GPAR, mbuf)
+        elif (cmdId == TUP_HHD_CMDID_SYS_UI_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_GPAR_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_GPAR_SAVE_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_GPAR_EXIT_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_GPAR_PIC_TRAIN_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_GPAR_PIC_FCC_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_MOMV_DIR_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_EXIT_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_MOFM_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_MOMV_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_MOMV_HOLEN_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_PILOT_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_PILOT_STOP_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_RIGHT_UP_SET_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_LEFT_BOT_SET_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CALI_PIC_CAP_HOLEN_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_PIC_CAP_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_PIC_CAP_STOP_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_PIC_CFY_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_PIC_CFY_STOP_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_MV_ZERO_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_FLU_CAP_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_FLU_CAP_STOP_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_FLU_CFY_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_CTRL_SCHD_FLU_CFY_STOP_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_MENG_START_REQ):
+            pass
+        elif (cmdId == TUP_HHD_CMDID_SYS_MENG_EXIT_REQ):
+            pass        
+        elif (cmdId == TUP_HHD_CMDID_SYS_MENG_COMMAND_REQ):
+            pass        
+        else:
+            print("CEBSHUICOBUS: Received un-specific command from UI!")
+            return TUP_FAILURE
         return TUP_SUCCESS
+
 
     def fsm_huicobus_gpar_get_cfg_resp_rcv_handler(self, msgContent):
         mbuf={}
