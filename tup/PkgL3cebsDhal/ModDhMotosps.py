a8047cd3c13b7c66ca9f55e5865630a1e4ce2943
Andyorhuateng <smdzjl@sina.cn>
2019-06-06 09:36:10 +0800

    LC:update Dhmotosps.py

--- tup/PkgL3cebsDhal/ModDhMotosps.py
+++ tup/PkgL3cebsDhal/ModDhMotosps.py
@@ -4,7 +4,14 @@ Created on 2019年6月3日
 @author: Administrator
 '''
 
+
 from PkgL3cebsDhal.cebsConfig import *
+
+import struct
+import serial
+import serial.tools.list_ports
+import sys
+
 
 class clsCebsDhMotosps():
     #
@@ -49,7 +56,12 @@ class clsCebsDhMotosps():
     SPS_TEST_PULES_CMID = 0x38
     SPS_SET_EXTI_DELAY_TIME_CMID = 0x39
     SPS_CHECK_PSWD_CMID = 0x40
-    #
+    
+    
+    SPS_MENGPAR_ADDR  = 0x77
+    SPS_MENGPAR_CMD_LEN = 18
+    
+    
     MOTOR_STEPS_PER_ROUND = 12800   #NF0
     MOTOR_DIS_MM_PER_ROUND = 3.1415926*20*1.05
     MOTOR_STEPS_PER_DISTANCE_MM = MOTOR_STEPS_PER_ROUND / MOTOR_DIS_MM_PER_ROUND
@@ -59,6 +71,161 @@ class clsCebsDhMotosps():
         super(clsCebsDhMotosps, self).__init__()  
     
 
+    
+    #给上层提供服务的函数
+    def funcInitSps(self):
+        #LC：ubuntu环境下，之前使用的那一套搜索设备符需要修改
+        # 现在使用的方案是创建udev rules文件来处理的
+        if sys.platform.startswith("linux"):
+            try:
+                self.serialFd = serial.Serial('/dev/cebsusb', 115200, timeout = 0.2)
+            except Exception:
+                self.IsSerialOpenOk = False
+                #self.funcMotoErrTrace("L2MOTO: Serial exist, but can't open!")
+                return -1
+            self.IsSerialOpenOk = True
+            #self.funcMotoLogTrace("L2MOTO: Success open serial port!")
+            return 1
+        
+        #windows下
+        if sys.platform.startswith("win32"):
+            plist = list(serial.tools.list_ports.comports())
+            self.targetComPortString = TUP_CEBS_SPS_USB_DBG_CARD2
+            self.drvVerNbr = -1
+            if len(plist) <= 0:
+                #self.funcMotoErrTrace("L2MOTO: Not serial device installed!")
+                return -2
+            else:
+                maxList = len(plist)
+                searchComPartString = ''
+                for index in range(0, maxList):
+                    self.medErrorLog("L2MOTO: " + str(plist[index]))
+                    plistIndex =list(plist[index])
+                    string = ("L2MOTO: Sps Init = ", plistIndex)
+                    self.tup_dbg_print(string)
+                    #Find right COM#
+                    for comPortStr in plistIndex:
+                        indexStart = comPortStr.find(self.targetComPortString)
+                        indexEnd = comPortStr.find(')')
+                        if (indexStart >= 0) and (indexEnd >=0) and (indexEnd > len(self.targetComPortString)):
+                            searchComPartString = comPortStr[len(self.targetComPortString):indexEnd]
+                if searchComPartString == '':
+                    #self.funcMotoErrTrace("L2MOTO: Can not find right serial port!")
+                    return -1
+                else:
+                    #self.funcMotoLogTrace("L2MOTO: Serial port is to open = " + str(searchComPartString))
+                    serialName = searchComPartString
+                try:
+                    self.serialFd = serial.Serial(serialName, 115200, timeout = 0.2)
+                except Exception:
+                    self.IsSerialOpenOk = False
+                    #self.funcMotoErrTrace("L2MOTO: Serial exist, but can't open!")
+                    return -1
+                self.IsSerialOpenOk = True
+                #self.funcMotoLogTrace("L2MOTO: Success open serial port!")
+                return 1
 
+    #命令打包
+    def funcSendCmdPack(self, cmdId, par1, par2, par3, par4):
+        #Build MODBUS COMMAND:系列化
+        #add for test pules  command ID 0x38   
+        if (cmdId == self.SPS_TEST_PULES_CMID):
+            for i in range(1, par1+1, 500):
+                fmt = ">BBiiii";
+                byteDataBuf = struct.pack(fmt, self.SPS_MENGPAR_ADDR, cmdId, i, par2, par3, par4)
+                crc = self.funcCacCrc(byteDataBuf, self.SPS_MENGPAR_CMD_LEN)
+                fmt = "<H";
+                byteCrc = struct.pack(fmt, crc)
+                byteDataBuf += byteCrc
+                #打印完整的BYTE系列
+                index=0
+                outBuf=''
+                while index < (self.SPS_MENGPAR_CMD_LEN+2):
+                    outBuf += str("%02X " % (byteDataBuf[index]))
+                    index+=1
+                #self.funcMotoLogTrace("L2MOTO: SND CMD = " + outBuf)
+                res, Buf = self.funcCmdSend(byteDataBuf)
+            return 1    
+        else:
+            fmt = ">BBiiii";    
+            byteDataBuf = struct.pack(fmt, self.SPS_MENGPAR_ADDR, cmdId, par1, par2, par3, par4)
+            crc = self.funcCacCrc(byteDataBuf, self.SPS_MENGPAR_CMD_LEN)
+            fmt = "<H";
+            byteCrc = struct.pack(fmt, crc)
+            byteDataBuf += byteCrc
+            #打印完整的BYTE系列
+            index=0
+            outBuf=''
+            while index < (self.SPS_MENGPAR_CMD_LEN+2):
+                outBuf += str("%02X " % (byteDataBuf[index]))
+                index+=1
+            #self.funcMotoLogTrace("L2MOTO: SND CMD = " + outBuf)
+            res, Buf = self.funcCmdSend(byteDataBuf)
+            if (res > 0):
+                return Buf
+            else:
+                return res
+            
+    #单条命令的执行
+    def funcCmdSend(self, cmd):
+        #正常状态
+        if(self.IsSerialOpenOk == False):
+            #self.funcMotoErrTrace("L2MOTO: Serial not opened, cant not send command!")
+            return -2,0
+        #串口的确已经被打开了
+        self.serialFd.readline()
+        self.serialFd.write(cmd)
+        rcvBuf = self.serialFd.readline()
+        if (len(rcvBuf) > 1):
+            while (rcvBuf[len(rcvBuf)-1] == 0x0A):
+                rcvBuf2 = self.serialFd.readline()
+                rcvBuf += rcvBuf2
+        length = len(rcvBuf)
+        if (length <=0):
+            #self.funcMotoErrTrace("L2MOTO: Nothing received. RCV BUF = " + str(rcvBuf))
+            return -3,0
+        outBuf = ''
+        for i in range(length):
+            outBuf += ("%02X "%(rcvBuf[i]))
+        #self.funcMotoLogTrace("L2MOTO: RCV BUF = " + outBuf)
+        #Check CRC
+        targetCrc = rcvBuf[length-2] + (rcvBuf[length-1]<<8)
+        rcvCrc = self.funcCacCrc(rcvBuf, length-2)
+        if (rcvCrc != targetCrc):
+            #self.funcMotoErrTrace("L2MOTO: Receive CRC Error!")
+            return -4,0
+        if (rcvBuf[0] != cmd[0]):
+            #self.funcMotoErrTrace("L2MOTO: Receive EquId Error!")
+            return -5,0
+        fmt = ">i";
+        upBuf = rcvBuf[3:7]
+        outPar = struct.unpack(fmt, upBuf)
+        return 1, outPar[0]
+    
+    #计算CRC
+    def funcCacCrc(self, buf, length):
+        wCRC = 0xFFFF;
+        index=0
+        while index < length:
+            wCRCOut = self.funcCrcOneChar(buf[index], wCRC)
+            wCRC = wCRCOut
+            index += 1
+        wHi = wCRC // 256;
+        wLo = wCRC % 256;
+        wCRC = (wHi << 8) | wLo;
+        return wCRC;
 
-
+    #计算CRC支持功能
+    def funcCrcOneChar(self, cDataIn, wCRCIn):
+        wCheck = 0;
+        wCRCIn = wCRCIn ^ cDataIn;
+        i=0;
+        while i<8:
+            wCheck = wCRCIn & 1;
+            wCRCIn = wCRCIn >> 1;
+            wCRCIn = wCRCIn & 0x7fff;
+            if (wCheck == 1):
+                wCRCIn = wCRCIn ^ 0xa001;
+            wCRCIn = wCRCIn & 0xffff;
+            i += 1
+        return wCRCIn;
