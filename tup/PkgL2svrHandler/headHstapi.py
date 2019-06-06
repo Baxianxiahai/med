990d422466607a5bdf870bd6d952def88d17de2e
Andyorhuateng <smdzjl@sina.cn>
2019-06-06 08:55:16 +0800

    zjl@2019/6/6 R1.68.7

--- tup/PkgL2svrHandler/headHstapi.py
+++ tup/PkgL2svrHandler/headHstapi.py
@@ -56,14 +56,10 @@ Created on 2019年6月4日
 #取存储参数 hstGetConfig
 TUP_HST_PCT_GET_CONFIG_IN = {
     'cmdid':'hstGetConfig',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_GET_CONFIG_OUT = {
     'cmdid':'hstGetConfig',
-    'par1':1,
-    'par2':2,
     'error_no': 'no_error',
     'cebs_cali_profile': {
         'platetype': ['1','2','5','6','7','8','9','10','11','12','13','15'],
@@ -110,8 +106,6 @@ TUP_HST_PCT_GET_CONFIG_OUT = {
 #设置存储参数 hstSetConfig
 TUP_HST_PCT_SET_CONFIG_IN = {
     'cmdid':'hstSetConfig',
-    'par1':1,
-    'par2':1,
     'cebs_cali_profile': {
         'platetype': ['1','2','5','6','7','8','9','10','11','12','13','15'],
         'calitime': '2010-05-06 20:00:00',
@@ -156,8 +150,6 @@ TUP_HST_PCT_SET_CONFIG_IN = {
 
 TUP_HST_PCT_SET_CONFIG_OUT = {
     'cmdid':'hstSetConfig',
-    'par1':1,
-    'par2':1,
     'error_no': 'no_error',
     'cebs_cali_profile': {
         'platetype': ['1','2','5','6','7','8','9','10','11','12','13','15'],
@@ -205,14 +197,10 @@ TUP_HST_PCT_SET_CONFIG_OUT = {
 #读取未识别图像参数 hstUnclfyPar
 TUP_HST_PCT_READ_UNCLFY_PAR_IN = {
     'cmdid':'hstReadUnclfyPar',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_READ_UNCLFY_PAR_OUT = {
     'cmdid':'hstReadUnclfyPar',
-    'par1':1,
-    'par2':1,
     }
 
 #更新校准参数  hstUpdateCaliPar
@@ -233,8 +221,6 @@ TUP_HST_PCT_UPDATE_CALI_PAR_IN = {
 
 TUP_HST_PCT_UPDATE_CALI_PAR_OUT = {
     'cmdid':'hstUpdateCaliPar',
-    'par1':1,
-    'par2':1,
     'error_no': 'no_error',
     'cebs_cali_profile': {
         'platetype': ['1','2','5','6','7','8','9','10','11','12','13','15'],
@@ -250,10 +236,7 @@ TUP_HST_PCT_UPDATE_CALI_PAR_OUT = {
 #增加批次号 hstAddBatchNbr
 TUP_HST_PCT_ADD_BATCH_NBR_IN = {
     'cmdid':'hstAddBatchNbr',
-    'par1':1,
-    'par2':1,
     'cebs_batch_info': {
-        'snbatch': 1000,
         'user': 'NameOfTheTester',
         'createtime': '2010-05-06 20:00:00',
         'comp_nbr': 123, #FFS
@@ -264,8 +247,6 @@ TUP_HST_PCT_ADD_BATCH_NBR_IN = {
 
 TUP_HST_PCT_ADD_BATCH_NBR_OUT = {
     'cmdid':'hstAddBatchNbr',
-    'par1':1,
-    'par2':1,
     'error_no': 'no_error',
     'cebs_batch_info': {
         'snbatch': 1000,
@@ -280,8 +261,6 @@ TUP_HST_PCT_ADD_BATCH_NBR_OUT = {
 #增加普通图像抓取 hstAddPicCap
 TUP_HST_PCT_ADD_PIC_CAP_IN = {
     'cmdid':'hstAddPicCap',
-    'par1':1,
-    'par2':1,
     'cebs_pvci_eleg': {
         'sid': 2000,
         'confid': 10,
@@ -290,26 +269,12 @@ TUP_HST_PCT_ADD_PIC_CAP_IN = {
         'file_attr':2,
         'name_before':'batch_20_hole_10_org.jpg',
         'cap_time':'2019-06-05 20:00:00',
-        'name_after':'batch_20_hole_10_cfy.jpg',
-        'rec_time':'2019-06-05 20:00:00',
-        'bigalive':20,
-        'bigdead':30,
-        'midalive':40,
-        'middead':20,
-        'smalive':10,
-        'smdead':5,
-        'totalalive':100,
-        'totaldead':50,
-        'totalsum':300,
-        'doneflag':1,
         'memo': 'This is for user specific comments',
         },
     }
 
 TUP_HST_PCT_ADD_PIC_CAP_OUT = {
     'cmdid':'hstAddPicCap',
-    'par1':1,
-    'par2':1,
     'error_no': 'no_error',
     'cebs_pvci_eleg': {
         'sid': 2000,
@@ -319,18 +284,6 @@ TUP_HST_PCT_ADD_PIC_CAP_OUT = {
         'file_attr':2,
         'name_before':'batch_20_hole_10_org.jpg',
         'cap_time':'2019-06-05 20:00:00',
-        'name_after':'batch_20_hole_10_cfy.jpg',
-        'rec_time':'2019-06-05 20:00:00',
-        'bigalive':20,
-        'bigdead':30,
-        'midalive':40,
-        'middead':20,
-        'smalive':10,
-        'smdead':5,
-        'totalalive':100,
-        'totaldead':50,
-        'totalsum':300,
-        'doneflag':1,
         'memo': 'This is for user specific comments',
         },
     }
@@ -338,22 +291,15 @@ TUP_HST_PCT_ADD_PIC_CAP_OUT = {
 #增加荧光图像抓取 hstAddFluCap
 TUP_HST_PCT_ADD_FLU_CAP_IN = {
     'cmdid':'hstAddFluCap',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_ADD_FLU_CAP_OUT = {
     'cmdid':'hstAddFluCap',
-    'par1':1,
-    'par2':1,
-
     }
 
 #更新普通图像识别 hstUpdatePicCfy
 TUP_HST_PCT_UPDATE_PIC_CFY_IN = {
     'cmdid':'hstUpdatePicCfy',
-    'par1':1,
-    'par2':1,
     'cebs_pvci_eleg': {
         'sid': 2000,
         'confid': 10,
@@ -380,8 +326,6 @@ TUP_HST_PCT_UPDATE_PIC_CFY_IN = {
 
 TUP_HST_PCT_UPDATE_PIC_CFY_OUT = {
     'cmdid':'hstUpdatePicCfy',
-    'par1':1,
-    'par2':1,
     'error_no': 'no_error',
     'cebs_pvci_eleg': {
         'sid': 2000,
@@ -410,30 +354,22 @@ TUP_HST_PCT_UPDATE_PIC_CFY_OUT = {
 #更新荧光图像识别 hstUpdateFluCfy
 TUP_HST_PCT_UPDATE_FLU_CFY_IN = {
     'cmdid':'hstUpdateFluCfy',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_UPDATE_FLU_CFY_OUT = {
     'cmdid':'hstUpdateFluCfy',
-    'par1':1,
-    'par2':1,
     }
 
 
 #读取普通图片 hstReadPic
 TUP_HST_PCT_READ_PIC_IN = {
     'cmdid':'hstReadPic',
-    'par1':1,
-    'par2':1,
     'batch_number': 100,
     'hole_number': 96,
     }
 
 TUP_HST_PCT_READ_PIC_OUT = {
     'cmdid':'hstReadPic',
-    'par1':1,
-    'par2':1,
     'error_no': 'no_error',
     'cebs_pvci_eleg': {
         'sid': 2000,
@@ -462,39 +398,41 @@ TUP_HST_PCT_READ_PIC_OUT = {
 #读取荧光图片 hstReadFlu
 TUP_HST_PCT_READ_FLU_IN = {
     'cmdid':'hstReadFlu',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_READ_FLU_OUT = {
     'cmdid':'hstReadFlu',
-    'par1':1,
-    'par2':1,
     }
-
 
 #更新图片统计 hstUpdateStatis
 TUP_HST_PCT_UPDATE_STATIS_IN = {
     'cmdid':'hstUpdateStatis',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_UPDATE_STATIS_OUT = {
     'cmdid':'hstUpdateStatis',
-    'par1':1,
-    'par2':1,
     }
 
 #更新用户日志 hstUpdateUserLog
 TUP_HST_PCT_UPDATE_USER_LOG_IN = {
     'cmdid':'hstUpdateUserLog',
-    'par1':1,
-    'par2':1,
     }
 
 TUP_HST_PCT_UPDATE_USER_LOG_OUT = {
     'cmdid':'hstUpdateUserLog',
-    'par1':1,
-    'par2':1,
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
